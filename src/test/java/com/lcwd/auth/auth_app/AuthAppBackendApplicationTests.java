a5b44d7fc200332d799f121b584e4a261764b9f8
ssoumya64 <sahoosoumya4862@gmail.com>
2025-12-20 11:39:44 +0530

    Initial commit - Spring Boot project setup

---
+++ src/test/java/com/lcwd/auth/auth_app/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.auth.auth_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
